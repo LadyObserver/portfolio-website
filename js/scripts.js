f61cf11e6452b40932d745ab5487f45f87fe45cf
Nikolett Muller <nikolett.muller@gmail.com>
2025-10-16 15:49:28 -0400

    Added dropdown on click
    Added dropdown on click event
    1. toggle on click
    2. close dropdown if clicking outside
    3. close dropdown when an option is chosen

--- js/scripts.js
+++ js/scripts.js
@@ -8,3 +8,28 @@ function menuToggle() {
     x.className = 'navtoggle';
   }
 }
+
+// Dropdown for case study map
+
+const dropdown = document.querySelector('.dropdown');
+const button = dropdown.querySelector('.dropbtn');
+const links = dropdown.querySelectorAll('.dropdown-content a');
+
+// Toggle dropdown on button click
+button.addEventListener('click', () => {
+  dropdown.classList.toggle('show');
+});
+
+// Close dropdown if clicking outside
+window.addEventListener('click', (e) => {
+  if (!dropdown.contains(e.target)) {
+    dropdown.classList.remove('show');
+  }
+});
+
+// Close dropdown when a link is clicked
+links.forEach(link => {
+  link.addEventListener('click', () => {
+    dropdown.classList.remove('show');
+  });
+});
